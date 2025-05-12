36288ec56d4be039317bc4c21d0791c2857958a9
Gargi Sarada Das <103807137+5547gargi@users.noreply.github.com>
2025-05-12 12:44:38 +0000

    updatedOnMon May 12 12:44:23 UTC 2025

--- daytodayactivity/purge_system_caches/1.sh
+++ daytodayactivity/purge_system_caches/1.sh
@@ -25,11 +25,11 @@ purging(){
 #function to generate report
 report(){
     local ip_address=$(curl -s -4 ident.me)
-    echo -e "\n\n\nServer $(ip_address)'s cache Purging report:- \n------ Before Purging -----/n$(cat ~old)\n\n--------After Purging---------/n$(free -wh)\nTimeStamp:- $(TZ='Asia/Kolkata' date)\n\n"
+    echo -e "\n\n\nServer $(ip_address)'s cache Purging report:- \n------ Before Purging -----\n$(cat ~old)\n\n--------After Purging---------\n$(free -wh)\nTimeStamp:- $(TZ='Asia/Kolkata' date)\n\n"
 }
 
 #record the memory usage before purging
 free -wh > ~/old
 
 #purge caches and generate report
-purging > dev/null 2>&1 && report || echo -e "Kindly run with sudo permission\nExample:- sudo $0"
+purging > /dev/null 2>&1 && report || echo -e "Kindly run with sudo permission\nExample:- sudo $0"
