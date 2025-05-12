088d42bb71d5d51204aec63062e0c732d63c1f43
Gargi Sarada Das <103807137+5547gargi@users.noreply.github.com>
2025-05-12 12:20:27 +0000

    updatedonMon May 12 12:20:14 UTC 2025

---
+++ daytodayactivity/purge_system_caches/1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Function to purge system caches
+purging(){
+    sudo sync
+    sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 2 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 2 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 2 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
+    sudo sync
+    sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
+}
+
+#function to generate report
+report(){
+    local ip_address=$(curl -s -4 ident.me)
+    echo -e "\n\n\nServer $(ip_address)'s cache Purging report:- \n------ Before Purging -----/n$(cat ~old)\n\n--------After Purging---------/n$(free -wh)\nTimeStamp:- $(TZ='Asia/Kolkata' date)\n\n"
+}
+
+#record the memory usage before purging
+free -wh > ~/old
+
+#purge caches and generate report
+purging > dev/null 2>&1 && report || echo -e "Kindly run with sudo permission\nExample:- sudo $0"
